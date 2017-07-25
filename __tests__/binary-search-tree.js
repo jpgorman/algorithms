96df7730782d45beba096108ab406f668df2e207
jeanpaul gorman <jean-paul.gorman@bluespeckfinancial.co.uk>
2017-07-25 11:54:36 +0100

    Add initial BST

---
+++ __tests__/binary-search-tree.js
@@ -0,0 +1,97 @@
+import {expect} from "chai"
+import {BinarySearchTree} from "../data-structures/binary-search-tree"
+
+describe("BinarySearchTree", function() {
+
+  describe("constructor", () => {
+    it("should create a current node", () => {
+      const expected = {
+        current: {
+          value: 5,
+          left: null,
+          right: null,
+        },
+      }
+      const actual = new BinarySearchTree(5)
+      expect(actual).to.eql(expected)
+    })
+  })
+
+  describe("insert", function() {
+    beforeEach(() => {
+      this.result = new BinarySearchTree(5)
+      this.result.insert(2)
+      this.result.insert(7)
+      this.result.insert(4)
+      this.result.insert(1)
+      this.result.insert(6)
+      this.result.insert(8)
+    })
+
+    it("should add values less than current to left", () => {
+      expect(this.result.current.left.value).to.eql(2)
+    })
+
+    it("should add values less than current to left recursively", () => {
+      expect(this.result.current.left.left.value).to.eql(1)
+    })
+
+    it("should add values less than current to left recursively", () => {
+      expect(this.result.current.left.right.value).to.eql(4)
+    })
+
+    it("should add values greater than current to right", () => {
+      expect(this.result.current.right.value).to.eql(7)
+    })
+
+    it("should add values greater than current to right recursively", () => {
+      expect(this.result.current.right.left.value).to.eql(6)
+    })
+
+    it("should add values greater than current to right recursively", () => {
+      expect(this.result.current.right.right.value).to.eql(8)
+    })
+  })
+
+  describe("contains", function() {
+    beforeEach(() => {
+      this.result = new BinarySearchTree(5)
+      this.result.insert(2)
+      this.result.insert(7)
+      this.result.insert(4)
+      this.result.insert(1)
+      this.result.insert(6)
+      this.result.insert(8)
+    })
+
+    it("should return early when value is found", () => {
+      expect(this.result.contains(5)).to.eql(true)
+    })
+
+    it("traverse to the left to find values less than current", () => {
+      expect(this.result.contains(2)).to.eql(true)
+    })
+
+    it("traverse to the right to find values greater than current", () => {
+      expect(this.result.contains(7)).to.eql(true)
+    })
+
+    it("traverse recursively to find values less than current", () => {
+      expect(this.result.contains(1)).to.eql(true)
+    })
+
+    it("traverse recursively to find values less than current", () => {
+      expect(this.result.contains(6)).to.eql(true)
+    })
+
+    it("traverse recursively to find values greater than current", () => {
+      expect(this.result.contains(4)).to.eql(true)
+    })
+
+    it("traverse recursively to find values greater than current", () => {
+      expect(this.result.contains(8)).to.eql(true)
+    })
+
+  })
+
+})
---
+++ data-structures/binary-search-tree.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value
+    this.left = left
+    this.right = right
+  }
+}
+
+export class BinarySearchTree {
+  constructor(value){
+    this.current = new Node(value)
+  }
+
+  insert(value, current) {
+    current = current || this.current
+    const direction = value < current.value ? "left" : "right"
+    if(current[direction] === null) {
+      current[direction] = new Node(value)
+    } else {
+      this.insert(value, current[direction])
+    }
+  }
+
+  contains(value, current) {
+    current = current || this.current
+    if (value === current.value)
+      return true
+    const direction = value < current.value ? "left" : "right"
+    return this.contains(value, current[direction])
+  }
+
+  /*
+  Invoke the callback for every node in a depth-first in-order (visit left branch, then current node, than right branch)
+  Note: In-Order traversal is most common type for binary trees. For binary search tree, this visits the nodes in ascending order (hence the name).
+  */
+
+  traverseDepthFirstInOrder() {
+
+  }
+}
